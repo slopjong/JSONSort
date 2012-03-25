c58b28acf0cbc7b2e82bdf7ef9a063e489540844
slopjong <slopjong>
2012-03-25 14:20:58 +0200

    Forked OpenSpaceLint and sorting the given JSON.

---
+++ c/js/reset.js
@@ -0,0 +1,13 @@
+/**
+ * Reset all the inputs and results
+ */
+
+$(document).ready(function(){
+
+    $("#json_input").val("");    
+    $("#add-space-input").val("");
+    $(".spec-result div").text("");
+    $(".spec-result pre").text("");
+    $(".spec-result").hide();
+    
+});
